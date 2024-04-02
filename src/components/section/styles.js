677cd77d498fe12c881b5db434065a56b3ae60da
Lennon Fonseca <Lennonfonsecaa@gmail.com>
2024-04-02 19:19:03 -0300

    section created

---
+++ src/components/section/styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    font-size: 3.2rem;
+    margin-bottom: 4.0rem;
+`
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,13 @@
 import { Container } from "./styles";
 import { Header } from "../../components/header";
+import { Section } from "../../components/section";
 
 
 export function Home() {
     return(
         <Container>
             <Header />
+            <Section title="Meus Filmes"/>
         </Container>
     )
 }
