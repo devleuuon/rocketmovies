162db963f7479fdbe821125853478abff5ebce82
Lennon Fonseca <Lennonfonsecaa@gmail.com>
2024-04-02 15:32:29 -0300

    first commit

---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+export default {
+    COLORS: {
+        PINK: "#FF859B",
+        WHITE: "#F4EDE8",
+        GRAY: "#CAC4CF",
+        BACKGROUND: "#312E38",
+        PLACEHOLDER: "#948F99",
+        BG_INPUT: "#262529",
+        BG_PROFILE: "#FF859B0D"
+    }
+}
