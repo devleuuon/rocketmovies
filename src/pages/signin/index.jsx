70ec3e89cddcb2fed85d7ea90887030d5e4b90fb
Lennon Fonseca <Lennonfonsecaa@gmail.com>
2024-07-05 12:08:09 -0300

    authenticated user

--- src/pages/signin/index.jsx
+++ src/pages/signin/index.jsx
@@ -4,8 +4,20 @@ import { Input } from '../../components/input'
 import { Button } from '../../components/button'
 import { FiMail, FiLock} from 'react-icons/fi'
 import { Link } from "react-router-dom";
+import { useAuth } from "../../hooks/auth";
+import { useState } from "react";
 
 export function SignIn() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const { SignIn } = useAuth()
+
+    function handleSignIn(){ //essa função vai para o botão de entrar.
+        SignIn({ email, password })
+    }
+
     return(
         <Container>
             <Form>
@@ -19,15 +31,17 @@ export function SignIn() {
                 type="email"
                 icon={FiMail}
                 placeholder="E-mail"
+                onChange={e => setEmail(e.target.value)}
                 />
 
                 <Input
                 type="password"
                 icon={FiLock}
                 placeholder="Senha"
+                onChange={e => setPassword(e.target.value)}
                 />
 
-                <Button title="Entrar"/>
+                <Button title="Entrar" onClick={handleSignIn}/>
 
 
                 <Link to="/rocketmovies/signup">Criar Conta</Link>
